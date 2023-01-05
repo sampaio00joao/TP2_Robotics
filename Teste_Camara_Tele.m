4eb4363b7cbb069540a5f2ddae94acb37317cac6
Tiago Carvalho <63639112+Tiag0Carvalh0@users.noreply.github.com>
2023-01-05 18:03:22 +0000

    Camara

--- Teste_Camara_Tele.m
+++ Teste_Camara_Tele.m
@@ -1,36 +1,41 @@
 clear all; close all; clc 
 %% SetUp
 %Instalar a aplicacao droidcam da appstore
-mycam = ipcam('http://100.65.178.26:4747/video'); % Colocar na consola 1x
-preview(mycam)
-%% Tirar e Guardar Foto
-pause(10)
-im = snapshot(mycam);
-imwrite(im,'CAM_PHOTO.jpg');
+% mycam = ipcam('http://100.65.178.26:4747/video'); % Colocar na consola 1x
+% preview(mycam)
+% %% Tirar e Guardar Foto
+% pause(10)
+% im = snapshot(mycam);
+% imwrite(im,'CAM_PHOTO.jpg');
+vid = webcam('HD Webcam');
+preview(vid);
+% closePreview(vid)
+im = snapshot(vid);
+figure, imshow(im);
 %% Aplicar Filtro Folha Branca
-bw = createMaskWhiteHSV(im); 
-%% Operações Morfológicas
-matrix = strel('square',15); % elemento estruturante
-closedIm = imclose(bw,matrix); % operação de fecho
-filledIm = imfill(closedIm,'holes'); % preenchimento do interior do Sinal
-%% Extração da área da folha
-stats =  regionprops(filledIm,'MajorAxisLength','MinorAxisLength','PixelIdxList','Area'); % retirar as areas de todos os blobs detetados na imagem
-% eliminar blobs indesejados / Ruído
-[maxValue,index] = max([stats.Area]); % guarda a área do maior blob / corresponde ao objeto 
-majorAxis = max([stats.MajorAxisLength]);
-minorAxis = max([stats.MinorAxisLength]);
-[rw col] = size(stats);
-for i=1:rw
-    if (i~=index)
-        filledIm(stats(i).PixelIdxList) = 0; % remove os blobs que correspondem ao ruído / área menor
-    end
-end
-%% Multiplicar as imagens
-multi = uint8(filledIm); % Converter para 8bits
-imMultiplied = im.*multi; % Multiplicar a Imagem Binaria com a Original
-figure, imshow(imMultiplied)
+% bw = createMaskWhiteHSV(im); 
+% %% Operações Morfológicas
+% matrix = strel('square',15); % elemento estruturante
+% closedIm = imclose(bw,matrix); % operação de fecho
+% filledIm = imfill(closedIm,'holes'); % preenchimento do interior do Sinal
+% %% Extração da área da folha
+% stats =  regionprops(filledIm,'MajorAxisLength','MinorAxisLength','PixelIdxList','Area'); % retirar as areas de todos os blobs detetados na imagem
+% % eliminar blobs indesejados / Ruído
+% [maxValue,index] = max([stats.Area]); % guarda a área do maior blob / corresponde ao objeto 
+% majorAxis = max([stats.MajorAxisLength]);
+% minorAxis = max([stats.MinorAxisLength]);
+% [rw col] = size(stats);
+% for i=1:rw
+%     if (i~=index)
+%         filledIm(stats(i).PixelIdxList) = 0; % remove os blobs que correspondem ao ruído / área menor
+%     end
+% end
+% %% Multiplicar as imagens
+% multi = uint8(filledIm); % Converter para 8bits
+% imMultiplied = im.*multi; % Multiplicar a Imagem Binaria com a Original
+% figure, imshow(imMultiplied)
 %% Aplicar Filtro Vermelho
-bw2 = createMaskRed(imMultiplied);
+bw2 = createMaskRed(im);
 figure, imshow(bw2)
 %% Operações Morfológicas
 matrix = strel('square',10); % elemento estruturante
@@ -63,9 +68,9 @@ pause(1);
 hold on
 plot(centroid(:,1), centroid(:,2), 'b*');
 %% Calcular posição 4.5cm base á ponta da folha 
-imSize = size(im);
-workAreaX = (imSize(1) - minorAxis) - minorAxis; 
-workAreaY = (imSize(2) - majorAxis) - majorAxis;
-locationX = workAreaX
-locationY = workAreaY
+imSize = size(im)
+%Tamnaho da folha A4 --> 21cm - 29.7
+objPosX = (centroid(:,1)*21) / imSize(:,1) %Em cm
+objPosY = (centroid(:,2)*29.7) / imSize(:,2) %Em cm
+
 
